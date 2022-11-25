3d5a26aad9b837a8b83b5de64b87e7b494dea5a2
Darpan Singh <01darpansingh08@gmail.com>
2022-11-25 10:02:44 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,12 +6,12 @@ def my_fibonacci(n):
    
     # Check if input is 0 then it will
     # print incorrect input
-    if n < 0:
-        print("Incorrect input")
+    # if n < 0:
+    #     print("Incorrect input")
  
     # Check if n is 0
     # then it will return 0
-    elif n == 0:
+    if n == 0:
         return 0
  
     # Check if n is 1,2
